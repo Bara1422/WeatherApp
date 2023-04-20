4b0532fcdf7dfb3f6ee0561c1e94cb6c2c1efcf7
Juan Baranovsky <xxbaraxx@hotmail.com>
2023-04-20 19:58:02 -0300

    add weather icons and responsive in this icons

--- src/components/ButtonsActualWeather.jsx
+++ src/components/ButtonsActualWeather.jsx
@@ -1,38 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import Uv from '../assets/UV'
+import Humidity from '../assets/Humidity'
+import Rain from '../assets/Rain'
 
-const isMdScreen = () => window.innerWidth >= 768
+const weatherIcons = {
+  humidity: <Humidity />,
+  precip: <Rain />,
+  uv: <Uv />
+}
+
 const ButtonsActualWeather = ({ current }) => {
-  const [isMd, setIsMd] = useState(isMdScreen())
+  const currentWeather = [
+    { name: 'humidity', value: `${current?.humidity}%` },
+    { name: 'precip', value: `${current?.precip_mm} mm` },
+    { name: 'uv', value: `${current?.uv} UV` }
+  ]
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMd(isMdScreen())
-    }
-    window.addEventListener('resize', handleResize)
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
+  console.log(current)
 
   return (
     <div className='flex justify-between gap-2 pt-4 text-sm md:text-base'>
-      {(isMd
-        ? [
-            `Humidity ${current?.humidity}%`,
-            `Precip. ${current?.precip_mm} mm`,
-            `UV ${current?.uv}`
-          ]
-        : [
-            `H ${current?.humidity}%`,
-            `P ${current?.precip_mm} mm`,
-            `UV ${current?.uv}`
-          ]
-      ).map((data, index) => (
+      {currentWeather.map((data, index) => (
         <div
-          className='sm:flex-none sm:w-1/5 sm:justify-center sm:flex sm:mx-auto flex-grow p-4 text-center text-blue-400 bg-gray-100 rounded-full basis-[30%]'
+          className='sm:w-1/5 sm:justify-center flex sm:mx-auto flex-col sm:flex-row p-4 text-center text-blue-400 font-semibold bg-gray-100 rounded-full basis-[30%] gap-2  mx-auto items-center'
           key={index}
         >
-          {data}
+          {weatherIcons[data.name]}
+          {data.value}
         </div>
       ))}
     </div>
